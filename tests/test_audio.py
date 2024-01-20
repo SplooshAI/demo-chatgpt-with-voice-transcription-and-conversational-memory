6c8256bbc8b6e82b94d98c0ebab4228b82c7b200
Rob Brennan <rob@therobbrennan.com>
2024-01-19 18:49:39 -0800

    Add tests for 42% coverage of the audio lib

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,24 +1,38 @@
 import pytest
-from unittest.mock import Mock
-from src.lib.audio import save_audio
-import os
+from unittest.mock import Mock, patch, MagicMock, mock_open
+from src.lib import audio
+import pyaudio
 import wave
 
-@pytest.fixture
-def audio_output_path(tmp_path):
-    return tmp_path / "test_audio.wav"
+def mock_wave_write():
+    wave_write = MagicMock()
+    wave_write.setnchannels.return_value = None
+    wave_write.setsampwidth.return_value = None
+    wave_write.setframerate.return_value = None
+    wave_write.writeframes.return_value = None
+    wave_write.close.return_value = None
+    return wave_write
 
-def test_save_audio_creates_correct_file_format(audio_output_path):
-    # Create mock data to simulate recorded frames
+def test_save_audio_creates_file(monkeypatch):
     mock_frames = [b'test'] * 10
     mock_pyaudio_instance = Mock()
     mock_pyaudio_instance.get_sample_size.return_value = 2
 
-    save_audio(mock_frames, mock_pyaudio_instance, str(audio_output_path), 'FORMAT', 1, 10000)
+    mock_wave = mock_wave_write()
+    with monkeypatch.context() as m:
+        m.setattr(wave, "open", mock_open())
+        m.setattr("wave.open", lambda *args, **kwargs: mock_wave)
+        audio.save_audio(mock_frames, mock_pyaudio_instance, 'test_output.wav', pyaudio.paInt16, 1, 10000)
+        mock_wave.setnchannels.assert_called_with(1)
+        mock_wave.setsampwidth.assert_called_with(2)
+        mock_wave.setframerate.assert_called_with(10000)
+        mock_wave.writeframes.assert_called_with(b'testtesttesttesttesttesttesttesttesttest')
+        mock_wave.close.assert_called()
 
-    # Verify if the file was created and is a valid wave file
-    assert os.path.isfile(audio_output_path)
-    with wave.open(str(audio_output_path), 'rb') as wf:
-        assert wf.getnchannels() == 1
-        assert wf.getsampwidth() == 2
-        assert wf.getframerate() == 10000
+def test_record_audio_in_test_mode(monkeypatch):
+    with monkeypatch.context() as m:
+        m.setattr("builtins.open", mock_open())
+        with patch("builtins.print") as mock_print:
+            audio.record_audio('test_output.wav', test_mode=True)
+            mock_print.assert_called_with("(!) EXITING EARLY: We do not actually record_audio during test mode (!)")
+            open.assert_called_once_with('test_output.wav', 'wb')
